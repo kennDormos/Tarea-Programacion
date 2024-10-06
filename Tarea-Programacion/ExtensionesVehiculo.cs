5b764f4a3040e4f5869f4253d953202274bcf1a6
Typeweb-dev <type.webdev@gmail.com>
2024-10-05 19:54:19 -0600

    creamos un metodo extension para mostrar los daros de nuestro objeto vehiculo. utilizando la palabra clave this para formatear los datos.

---
+++ Tarea-Programacion/ExtensionesVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Programacion
+{
+    public static class ExtensionesVehiculo
+    {
+        public static void MostrarInfo(this Vehiculo vehiculo)
+        {
+            Console.WriteLine($"Modelo: {vehiculo.Modelo}, Precio por día: {vehiculo.PrecioPorDia:C}");
+        }
+    }
+}
