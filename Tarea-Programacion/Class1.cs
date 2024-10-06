3f5bd7482549b5adbc477f1a19c108184ac6ae69
Typeweb-dev <type.webdev@gmail.com>
2024-10-05 19:28:41 -0600

    Creamos una clase llamada Vehiculo con propiedades modelo y precio por dia.

---
+++ Tarea-Programacion/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Programacion
+{
+    public class Vehiculo
+    {
+        public string Modelo { get; set; }
+        public double PrecioPorDia { get; set; }
+
+    }
+}
