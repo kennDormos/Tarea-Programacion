37cdc168f5087dc24ffd460668983716090510bc
Typeweb-dev <type.webdev@gmail.com>
2024-10-05 20:27:38 -0600

    En la clase principal creamos una instancia de Vehiculo, aplicamos un descuento, mostramos la informacion del objeto y calculamos el costo total de reserva mendiante el metodo calcular costo total de clase statica calculaReserva, al final imprimimos el costo total.

--- Tarea-Programacion/Program.cs
+++ Tarea-Programacion/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace Tarea_Programacion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vehiculo vehiculo = new Vehiculo { Modelo = "Toyota Corolla", PrecioPorDia = 99.9 };
+            
+            double porcentajeDescuento = 10.0;
+            
+            vehiculo.MostrarInfo();
+
+            double costoTotal = CalcularReserva.CalcularCostoTotal(vehiculo, 5, porcentajeDescuento);
+
+            Console.WriteLine($"El costo total de reserva de 5 dias con un descuento del {porcentajeDescuento}% es de:{costoTotal}");
+        }
+    }
+}
