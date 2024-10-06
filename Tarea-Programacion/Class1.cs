33b2eb2b553fe0ac96c24cc8f9357b170c4e9dd0
Typeweb-dev <type.webdev@gmail.com>
2024-10-05 19:42:46 -0600

    Creamos una clase interna dentro de la clase vehiculos con un metodo para aplicar el descuento al precio del vehiculo.

--- Tarea-Programacion/Class1.cs
+++ Tarea-Programacion/Class1.cs
@@ -11,5 +11,12 @@ namespace Tarea_Programacion
         public string Modelo { get; set; }
         public double PrecioPorDia { get; set; }
 
+        public class Descuento
+        {
+            public static double AplicarDescuento(double precio,double porcentajedescuento)
+            {
+                return precio - (precio * (porcentajedescuento / 100));
+            }
+        }
     }
 }
