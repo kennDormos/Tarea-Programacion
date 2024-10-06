a2e1aefe8171f551f39483155994854757a92ffa
Typeweb-dev <type.webdev@gmail.com>
2024-10-05 20:09:23 -0600

    creamos una clase estatica llamada calcularReserva la cual toma tres parametros y utiliza el metodo de la clase descuento para calcular el precio de con descuento y luego se multiplica por dias.

---
+++ Tarea-Programacion/CalcularReserva.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Programacion
+{
+    public static class CalcularReserva
+    {
+        public static double CalcularCostoTotal(Vehiculo vehiculo,int dias, double porcentajedescuento)
+        {
+            double precioConDescuento = Vehiculo.Descuento.AplicarDescuento(vehiculo.PrecioPorDia, porcentajedescuento);
+            return precioConDescuento * dias;
+        }
+    }
+}
